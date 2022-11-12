15819b354dfcc8da1e6d0505e9a19978f57b8694
Alexander Gerstberger <a.gerstberger@web.de>
2022-11-12 15:35:57 +0100

    hinzufügen von Auftrag struct und Funktion zum Auslesen von Aufträgen aus einer Datei in eine Liste

---
+++ Aufgabe4/fahrradwerkstatt/Auftrag.go
@@ -0,0 +1,41 @@
+package fahrradwerkstatt
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Auftrag struct {
+	Eingangszeitpunkt int // der Eingangszeitpunkt des Auftrags in Minuten
+	Bearbeitungsdauer int // die Bearbeitungsdauer des Auftrags in Minuten
+}
+
+func AuftraegeAusDateiLesen(filename string) []Auftrag {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+
+	var auftraege []Auftrag
+	for fileScanner.Scan() {
+		line := strings.Split(fileScanner.Text(), " ")
+		if len(line) != 2 {
+			continue
+		}
+		eingangszeitpunkt, _ := strconv.Atoi(line[0])
+		bearbeitungsdauer, _ := strconv.Atoi(line[1])
+
+		auftraege = append(auftraege, Auftrag{
+			Eingangszeitpunkt: eingangszeitpunkt,
+			Bearbeitungsdauer: bearbeitungsdauer,
+		})
+	}
+
+	return auftraege
+}
