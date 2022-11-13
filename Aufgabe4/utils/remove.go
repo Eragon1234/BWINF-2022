9f13cfd43edf001170c280eb9461f37b5ff837b6
Alexander Gerstberger <a.gerstberger@web.de>
2022-11-13 10:57:18 +0100

    verhindern von Fehlern bei Remove und RemoveAndReturn bei nicht existierendem Index

--- Aufgabe4/utils/remove.go
+++ Aufgabe4/utils/remove.go
@@ -1,11 +1,17 @@
 package utils
 
 func Remove[T any](a []T, i int) []T {
+	if len(a) <= i {
+		return a
+	}
 	a = append(a[:i], a[i+1:]...)
 	return a
 }
 
 func RemoveAndReturn[T any](a []T, i int) ([]T, T) {
+	if len(a) <= i {
+		return a, *new(T)
+	}
 	removedItem := a[i]
 	a = Remove(a, i)
 	return a, removedItem
