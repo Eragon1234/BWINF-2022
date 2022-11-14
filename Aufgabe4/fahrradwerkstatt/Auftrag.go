78f4f0e2065d25e205712cb2f39e3a1e5c411fd7
Alexander Gerstberger <a.gerstberger@web.de>
2022-11-14 08:19:59 +0100

    hinzufügen von Restdauer zu Auftrag und Nutzung von Pointern im Return der AuftraegeAusDateiLesen Funktion

--- Aufgabe4/fahrradwerkstatt/Auftrag.go
+++ Aufgabe4/fahrradwerkstatt/Auftrag.go
@@ -9,11 +9,13 @@ import (
 )
 
 type Auftrag struct {
-	Eingangszeitpunkt int // der Eingangszeitpunkt des Auftrags in Minuten
-	Bearbeitungsdauer int // die Bearbeitungsdauer des Auftrags in Minuten
+	Eingangszeitpunkt        int // der Eingangszeitpunkt des Auftrags in Minuten
+	Bearbeitungsdauer        int // die Bearbeitungsdauer des Auftrags in Minuten
+	Restdauer                int // die Restdauer des Auftrags am aktuellen Zeitpunkt
+	Fertigstellungszeitpunkt int // der Fertigstellungszeitpunkt des Auftrags in Minuten
 }
 
-func AuftraegeAusDateiLesen(filename string) []Auftrag {
+func AuftraegeAusDateiLesen(filename string) []*Auftrag {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -22,7 +24,7 @@ func AuftraegeAusDateiLesen(filename string) []Auftrag {
 
 	fileScanner := bufio.NewScanner(file)
 
-	var auftraege []Auftrag
+	var auftraege []*Auftrag
 	for fileScanner.Scan() {
 		line := strings.Split(fileScanner.Text(), " ")
 		if len(line) != 2 {
@@ -31,7 +33,7 @@ func AuftraegeAusDateiLesen(filename string) []Auftrag {
 		eingangszeitpunkt, _ := strconv.Atoi(line[0])
 		bearbeitungsdauer, _ := strconv.Atoi(line[1])
 
-		auftraege = append(auftraege, Auftrag{
+		auftraege = append(auftraege, &Auftrag{
 			Eingangszeitpunkt: eingangszeitpunkt,
 			Bearbeitungsdauer: bearbeitungsdauer,
 		})
