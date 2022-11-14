70f81ebddc1d3944f7622632e493c4f63de4194c
Alexander Gerstberger <a.gerstberger@web.de>
2022-11-14 20:43:12 +0100

    hinzufügen von simplen Tests für sortArrayByShortest Funktion

--- Aufgabe4/fahrradwerkstatt/fahrradwerkstatt_test.go
+++ Aufgabe4/fahrradwerkstatt/fahrradwerkstatt_test.go
@@ -1,6 +1,9 @@
 package fahrradwerkstatt
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAeltesterAuftragZuerst(t *testing.T) {
 	type args struct {
@@ -121,6 +124,98 @@ func TestKuerzesterAuftragZuerst(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("KuerzesterAuftragZuerst() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_sortArrayByShortest(t *testing.T) {
+	type args struct {
+		array []*Auftrag
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*Auftrag
+	}{
+		{
+			name: "testing array with two elements not correctly sorted",
+			args: args{
+				array: []*Auftrag{
+					{
+						Eingangszeitpunkt:        9 * 60,
+						Bearbeitungsdauer:        200,
+						Restdauer:                200,
+						Fertigstellungszeitpunkt: 0,
+					},
+					{
+						Eingangszeitpunkt:        9*60 + 24*60,
+						Bearbeitungsdauer:        100,
+						Restdauer:                100,
+						Fertigstellungszeitpunkt: 0,
+					},
+				},
+			},
+			want: []*Auftrag{
+				{
+					Eingangszeitpunkt:        9*60 + 24*60,
+					Bearbeitungsdauer:        100,
+					Restdauer:                100,
+					Fertigstellungszeitpunkt: 0,
+				},
+				{
+					Eingangszeitpunkt:        9 * 60,
+					Bearbeitungsdauer:        200,
+					Restdauer:                200,
+					Fertigstellungszeitpunkt: 0,
+				},
+			},
+		},
+		{
+			name: "testing array with two elements already correctly sorted",
+			args: args{
+				array: []*Auftrag{
+					{
+						Eingangszeitpunkt:        9*60 + 24*60,
+						Bearbeitungsdauer:        100,
+						Restdauer:                100,
+						Fertigstellungszeitpunkt: 0,
+					},
+					{
+						Eingangszeitpunkt:        9 * 60,
+						Bearbeitungsdauer:        200,
+						Restdauer:                200,
+						Fertigstellungszeitpunkt: 0,
+					},
+				},
+			},
+			want: []*Auftrag{
+				{
+					Eingangszeitpunkt:        9*60 + 24*60,
+					Bearbeitungsdauer:        100,
+					Restdauer:                100,
+					Fertigstellungszeitpunkt: 0,
+				},
+				{
+					Eingangszeitpunkt:        9 * 60,
+					Bearbeitungsdauer:        200,
+					Restdauer:                200,
+					Fertigstellungszeitpunkt: 0,
+				},
+			},
+		},
+		{
+			name: "testing empty array",
+			args: args{
+				array: []*Auftrag{},
+			},
+			want: []*Auftrag{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortArrayByShortest(tt.args.array); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortArrayByShortest() = %v, want %v", got, tt.want)
 			}
 		})
 	}
