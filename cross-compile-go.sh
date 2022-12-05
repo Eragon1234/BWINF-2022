877aa46a06145fc6e1db5de56e71578db19ac002
Alexander Gerstberger <a.gerstberger@web.de>
2022-12-05 11:47:28 +0100

    cross-compile-go.sh schneller machen, indem die for Schleife parallel ausgeführt wird

--- cross-compile-go.sh
+++ cross-compile-go.sh
@@ -11,5 +11,5 @@ for dist in "${dists[@]}"; do
   if [[ ! " ${gooses[*]} " =~ ${D[0]} ]]; then
     continue
   fi
-  (cd "${path}" && GOOS="${D[0]}" GOARCH="${D[1]}" go build -o "../AusfuehrbaresProgramm/${D[0]}-${D[1]}")
+  (cd "${path}" && GOOS="${D[0]}" GOARCH="${D[1]}" go build -o "../AusfuehrbaresProgramm/${D[0]}-${D[1]}") &
 done
